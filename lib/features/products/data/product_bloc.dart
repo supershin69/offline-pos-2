38cb3502bbff95763377497d67aa0ecf37616c91
shiro <mgshinthant58@gmail.com>
2026-07-16 21:06:01 +0630

    Database modification

--- lib/features/products/data/product_bloc.dart
+++ lib/features/products/data/product_bloc.dart
@@ -1,9 +1,9 @@
-// Events
+// features/products/data/product_bloc.dart
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:offline_pos/core/database/database.dart';
-import 'package:offline_pos/features/products/models/item_with_quantity.dart';
 import 'package:offline_pos/features/products/repositories/product_repository.dart';
 
+// --- Events ---
 abstract class ProductEvent {}
 
 class MonitorProductStarted extends ProductEvent {}
@@ -13,37 +13,34 @@ class AddProductRequested extends ProductEvent {
   AddProductRequested({required this.item});
 }
 
-class AddVariantRequested extends ProductEvent {
-  final VariantsCompanion variant;
-  AddVariantRequested({required this.variant});
-}
+// ❌ AddVariantRequested Event ကို ဖြုတ်လိုက်ပါပြီ
 
-// States
+// --- States ---
 abstract class ProductState {}
 
 class ProductInitial extends ProductState {}
 
 class ProductLoaded extends ProductState {
-  final List<ItemWithQuantity> products;
+  final List<Item> products; // ◄ Item စာရင်းသက်သက်ပဲ သယ်ဆောင်တော့တယ်
   ProductLoaded({required this.products});
 }
 
-// Bloc
-
+// --- BLoC Logic ---
 class ProductBloc extends Bloc<ProductEvent, ProductState> {
   final ProductRepository _repository;
+
   ProductBloc(this._repository) : super(ProductInitial()) {
     on<MonitorProductStarted>((event, emit) async {
-      await emit.forEach<List<ItemWithQuantity>>(
+      await emit.forEach<List<Item>>(
         _repository.watchProducts(),
         onData: (itemList) => ProductLoaded(products: itemList),
       );
     });
+
     on<AddProductRequested>((event, emit) async {
       await _repository.addProduct(event.item);
     });
-    on<AddVariantRequested>((event, emit) async {
-      await _repository.addVariant(event.variant);
-    });
+
+    // ❌ on<AddVariantRequested> ကို ဖြုတ်ပစ်လိုက်ပါပြီ
   }
 }
--- lib/features/products/models/item_with_quantity.dart
+++
@@ -1,8 +0,0 @@
-import 'package:offline_pos/core/database/database.dart';
-
-class ItemWithQuantity {
-  final Item item;
-  final int quantity;
-
-  ItemWithQuantity({required this.item, required this.quantity});
-}
